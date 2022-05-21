371fec80f4e8a1d48b4a376693648ec4c0d00ede
Ian Maynard <38901355+Relreo@users.noreply.github.com>
2022-05-21 16:34:12 -0700

    Finished File Creation Menu
    File Creation works, but needs to be more deeply tested for bugs. storage for file should be set when its opened or created. Need to work on data visualization ideas soon...

--- JSONWizard.py
+++ JSONWizard.py
@@ -1,6 +1,6 @@
 # Import Statements
 from fileinput import filename
-from PySide2.QtGui import QIcon
+from PySide2.QtGui import QIcon, QCloseEvent
 from PySide2.QtCore import QSize, Qt, QFile, QTextStream
 from PySide2.QtWidgets import QStyle, QPushButton, QLabel, QHBoxLayout, QApplication, QAction, QWidget, QMainWindow, QToolBar, QFileDialog, QFormLayout, QLineEdit
 import sys
@@ -9,11 +9,88 @@ from pathvalidate import ValidationError, validate_filename
 # QApplication Instance
 app = QApplication([])
 
-# Set up Style Sheet
+# Set up general Style Sheet
 styleSheetFile = QFile("./qss/stylesheet.qss")
 styleSheetFile.open(QFile.ReadOnly | QFile.Text)
 styleSheet = QTextStream(styleSheetFile)
 app.setStyleSheet(styleSheet.readAll())
+
+class JSONWizard(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        # Currently Open File
+        self.openFile = None
+        self.openFilePath = None
+        self.fileCurrentlyOpen = False
+        # Set up popup create file window
+        self.createFileWindow = None
+
+        # Window Title and Size
+        self.setWindowTitle("JSONWizard")
+        self.setMinimumSize(980, 640)
+        appIcon = QIcon("./icons/appIcon.png")
+        self.setWindowIcon(appIcon)
+
+        # Create Menu Bar
+        self.setUpMenuBar()
+        # Create Tool Bar
+        toolBar = QToolBar()
+        toolBar.setIconSize(QSize(64,64))
+        toolBar.setMovable(False)
+        toolBar.setMinimumHeight(25)
+        
+        
+        self.addToolBar(toolBar)
+        # TODO Set up Visual Editor
+    
+    def setUpMenuBar(self):
+        menu = self.menuBar()
+        # Set Up File Menu
+        file_menu = menu.addMenu("File")
+        # Menu Item for creating new JSON files
+        createFileAction = QAction(QIcon("./icons/JSONFile.png"), "Create new JSON file...", self)
+        createFileAction.triggered.connect(self.openCreateFileMenu)
+        # Menu Item for opening existing JSON files
+        openFileAction = QAction(QIcon("./icons/JSONFile.png"), "Open existing JSON file...", self)
+        openFileAction.triggered.connect(self.openFileExplorer)
+        # TODO Menu Item for Saving the Current File (with hotkey Ctrl-S)
+
+        # TODO Menu Item for Saving the current file as a new file (with hotkey Shift-Ctrl-S)
+
+        # TODO Menu Item for Converting from JSON to another file type?
+
+        # Add all menu items
+        file_menu.addAction(createFileAction)
+        file_menu.addAction(openFileAction)
+
+        # TODO Set Up Help Menu
+        help_menu = menu.addMenu("Help")
+
+    def openCreateFileMenu(self):
+        if self.createFileWindow is None:
+            self.createFileWindow = FileCreationWindow()
+        self.createFileWindow.show()
+    
+    def openFileExplorer(self):
+        fileTuple = QFileDialog.getOpenFileName(self, "Open JSON File", "./", "JSON Files (*.json)")
+        fileNameAndPath = fileTuple[0]
+        self.fileCurrentlyOpen = True
+        try:
+            self.openFile = open(fileNameAndPath)
+        except:
+            print("WEIRD OS ERROR OCCURED MADAY MADAY *HORNS SOUNDING*")
+        else:
+            self.openFilePath = fileNameAndPath
+
+    def saveCurrentFile(self):
+        pass
+
+    def saveAsCurrentFile(self):
+        pass
+
+mainPage = JSONWizard()
+mainPage.show()
 
 class FileCreationWindow(QWidget):
     def __init__(self):
@@ -33,6 +110,7 @@ class FileCreationWindow(QWidget):
         layout = QFormLayout()
         layout.setMargin(80)
         layout.setVerticalSpacing(50)
+    
 
         # Create and Configure File Name Field
         hbox = QHBoxLayout()
@@ -46,8 +124,9 @@ class FileCreationWindow(QWidget):
         self.fileExtensionLabel = QLabel(" .json")
         self.fileExtensionLabel.setScaledContents(True)
         hbox.addWidget(self.fileExtensionLabel)
-        # Add to Layout
+    
         layout.addRow("Save As: ", hbox)
+        
 
         # Create and Configure File Path Field and Selection Button
         hbox2 = QHBoxLayout()
@@ -87,28 +166,63 @@ class FileCreationWindow(QWidget):
         self.confirmButton.setFixedHeight(buttonHeight)
         self.confirmButton.clicked.connect(self.validateInput)
         hbox3.addWidget(self.confirmButton)
-        # Add to layout
+
         layout.addRow(hbox3)
+
+        # Create Error Label that displays when something goes wrong
+        self.errorLabel = QLabel("ERROR LABEL")
+        self.errorLabel.setAlignment(Qt.AlignCenter)
+        self.errorLabel.setStyleSheet("color: red; font-size: 16px")
+        self.errorLabel.hide()
+
+
+        layout.addRow(self.errorLabel)
         # Set Layout of the Window
         self.setLayout(layout)
     
     # Function to validate entered file name and selected path
     def validateInput(self):
         fileName = self.fileNameField.text()
-        if not fileName:
-            # Error Handling!
-            pass
         path = self.filePathField.text()
-        if not path:
-            # Error Handling!
-            pass
+        if len(fileName) == 0:
+            self.errorLabel.setText("*Error: Please enter a file name!*")
+            self.errorLabel.show()
+        elif len(path) == 0:
+            self.errorLabel.setText("*Error: Please select a file path!*")
+            self.errorLabel.show()
+        else:
+            try:
+                fileName += ".json"
+                validate_filename(fileName)
+            except ValidationError:
+                self.errorLabel.setText("*Error: Please enter a VALID file name!*")
+                self.errorLabel.show()
+            else:
+                self.errorLabel.hide()
+                self.createNewJsonFile(fileName, path)
+
+
+
             
     # Function to Create New JSON Files
-    def createNewJsonFile(self, filePath):
+    def createNewJsonFile(self, fileName, filePath):
         # Create New JSON File
-
-        pass
-
+        fileNameAndPath = filePath + "/" + fileName
+        temp = None
+        try:
+            temp = open(fileNameAndPath, 'x')
+        except FileExistsError:
+            self.errorLabel.setText("*Error: File already exists!*")
+            self.errorLabel.show()
+        except:
+            self.errorLabel.setText("*WEIRD OS ERROR, SHOULD BE UNREACHABLE*")
+            self.errorLabel.show()
+        else:
+            mainPage.openFile = temp
+            mainPage.openFilePath = fileNameAndPath
+            mainPage.fileCurrentlyOpen = True
+            self.close()
+            
     # Function to open screen where user can select the directory for their new file
     def openPathSelectionScreen(self):
         dialog = QFileDialog(self)
@@ -117,92 +231,12 @@ class FileCreationWindow(QWidget):
             directoryName = dialog.selectedFiles()[0]
             self.filePathField.setText(directoryName)
     
-            
-
-
-# Main Program Window
-class JSONWizard(QMainWindow):
-    
-    def __init__(self):
-        super().__init__()
-        # Currently Open File
-        self.openFile = None
-        self.openFilePath = None
-        self.fileCurrentlyOpen = False
-        # Set up popup create file window
-        self.createFileWindow = None
-
-        # Window Title and Size
-        self.setWindowTitle("JSONWizard")
-        self.setMinimumSize(980, 640)
-        appIcon = QIcon("./icons/appIcon.png")
-        self.setWindowIcon(appIcon)
-
-        # Create Menu Bar
-        self.setUpMenuBar()
-        # Create Tool Bar
-        toolBar = QToolBar()
-        toolBar.setIconSize(QSize(64,64))
-        toolBar.setMovable(False)
-        toolBar.setMinimumHeight(25)
-        
-        
-        self.addToolBar(toolBar)
-        # Set up Visual Editor
-    
-    def setUpMenuBar(self):
-        menu = self.menuBar()
-        # Set Up File Menu
-        file_menu = menu.addMenu("File")
-        # Menu Item for creating new JSON files
-        createFileAction = QAction(QIcon("./icons/JSONFile.png"), "Create new JSON file...", self)
-        createFileAction.triggered.connect(self.openCreateFileMenu)
-        # Menu Item for opening existing JSON files
-        openFileAction = QAction(QIcon("./icons/JSONFile.png"), "Open existing JSON file...", self)
-        openFileAction.triggered.connect(self.openFileExplorer)
-        # Menu Item for Saving the Current File (with hotkey Ctrl-S)
-
-        # Menu Item for Saving the current file as a new file (with hotkey Shift-Ctrl-S)
-
-        # Menu Item for Converting from JSON to another file type?
-
-        # Add all menu items
-        file_menu.addAction(createFileAction)
-        file_menu.addAction(openFileAction)
-
-        # Set Up Help Menu
-        help_menu = menu.addMenu("Help")
-
-        # file_menu.addAction()
-
-    def openCreateFileMenu(self):
-        if self.createFileWindow is None:
-            self.createFileWindow = FileCreationWindow()
-        self.createFileWindow.show()
-        
-    
-    def openFileExplorer(self):
-        fileTuple = QFileDialog.getOpenFileName(self, "Open JSON File", "./", "JSON Files (*.json)")
-        self.openFilePath = fileTuple[0]
-        self.fileCurrentlyOpen = True
-
-    def saveCurrentFile(self):
-        pass
-
-    def saveAsCurrentFile(self):
-        pass
-
-    
-        
-
-        
-
-mainPage = JSONWizard()
-mainPage.show()
-
-
-
-
+    # Function to Wipe the Form after it is closed
+    def closeEvent(self, event: QCloseEvent) -> None:
+        super().closeEvent(event)
+        self.errorLabel.hide()
+        self.fileNameField.clear()
+        self.filePathField.clear()
 
 # Start App Loop, Code Below this will not execute until application is exited
 app.exec_()
